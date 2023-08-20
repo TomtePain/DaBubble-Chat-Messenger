b4245e48f7c4c45aa14c7cf6e95d444cd9755ce9
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-08-20 09:19:07 +0200

    connection to firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'da-bubble-own',
+    appId: '1:199785897732:web:45d55fca274013c6147a51',
+    storageBucket: 'da-bubble-own.appspot.com',
+    apiKey: 'AIzaSyDmG43qF_eSj30iFKkpwel-lWiLGTUpcj8',
+    authDomain: 'da-bubble-own.firebaseapp.com',
+    messagingSenderId: '199785897732',
+  },
+};
