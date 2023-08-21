6c416d5f64cee2a2c01b982339038b346f5dea79
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-08-21 09:35:22 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { GeneralViewComponent } from './pages/general-view/general-view.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { ThreadComponent } from './components/thread/thread.component';
 import { WorkspceComponent } from './components/workspce/workspce.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -27,7 +32,11 @@ import { HeaderComponent } from './components/header/header.component';
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    CdkTreeModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
