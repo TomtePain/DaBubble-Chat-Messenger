e820649c1952f33bd9f3554373d9f3a95fc51bb2
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-11-01 20:50:59 +0100

    update to own api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyDmG43qF_eSj30iFKkpwel-lWiLGTUpcj8",
-    authDomain: "da-bubble-own.firebaseapp.com",
-    projectId: "da-bubble-own",
-    storageBucket: "da-bubble-own.appspot.com",
-    messagingSenderId: "199785897732",
-    appId: "1:199785897732:web:45d55fca274013c6147a51"
+    projectId: 'da-bubble-own',
+    appId: '1:199785897732:web:45d55fca274013c6147a51',
+    storageBucket: 'da-bubble-own.appspot.com',
+    apiKey: 'AIzaSyDmG43qF_eSj30iFKkpwel-lWiLGTUpcj8',
+    authDomain: 'da-bubble-own.firebaseapp.com',
+    messagingSenderId: '199785897732',
   },
   userDb: 'user',
   channelDb: 'channel',
