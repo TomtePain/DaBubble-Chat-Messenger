b1cc2518763d5e4e0eb083305b992630deec1b39
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-11-07 17:51:05 +0100

    Update Authentification Service and ChannelDB

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Auth, GoogleAuthProvider, createUserWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, signInAnonymously, signInWithEmailAndPassword, signInWithPopup, signOut } from '@angular/fire/auth';
-import { Firestore, collection, doc, docData, getDocs, query, setDoc, where } from '@angular/fire/firestore';
+import { DocumentReference, Firestore, arrayUnion, collection, doc, docData, getDocs, query, setDoc, updateDoc, where } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { AlertService } from '../auth/components/alert/alert.service';
+import { CrudService } from '../main/services/crud.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  loggedUserId:string = '';  
-  constructor(public auth: Auth, private firestore: Firestore, private route: Router, private alertService:AlertService) { }
+  loggedUserId: string = '';
+
+  constructor(public auth: Auth, private firestore: Firestore, private route: Router, private alertService: AlertService, private crud: CrudService) { }
 
   regUser(regFormValue: any, photoURL: string, accessToChannel: any, isOnline: boolean) {
     const { email, password, fullName } = regFormValue.value;
@@ -21,7 +23,7 @@ export class AuthService {
       const userId = resp.user.uid;
       const itemCollection = collection(this.firestore, environment.userDb);
       sendEmailVerification(resp.user);
-      setDoc(doc(itemCollection), { uid: userId, fullName: fullName, photoURL: photoURL, accessToChannel: [], isOnline: false, email:email });
+      setDoc(doc(itemCollection), { uid: userId, fullName: fullName, photoURL: photoURL, accessToChannel: [], isOnline: false, email: email });
       this.alert('Konto erfolgreich erstellt!');
       this.route.navigateByUrl('auth/login');
     }).catch(err => {
@@ -29,12 +31,14 @@ export class AuthService {
     })
   }
 
+
   sendPasswortResetMail(email: string) {
     sendPasswordResetEmail(this.auth, email).then(resp => {
       this.alert('E-Mail gesendet');
-
     })
   }
+
+
   googleWithAuth() {
     signInWithPopup(this.auth, new GoogleAuthProvider()).then(async resp => {
       const userId = resp.user.uid;
@@ -43,23 +47,41 @@ export class AuthService {
       const email = resp.user.email;
       const itemCollection = collection(this.firestore, environment.userDb);
       let userIsReg = await this.isRegUser(userId);
-      
+      let userData = { uid: userId, fullName: fullName, photoURL: userPhotoURL, accessToChannel: [], isOnline: false, email: email };
+
       if (userIsReg.length > 0) {
-        setDoc(doc(itemCollection, userIsReg), { uid: userId, fullName: fullName, photoURL: userPhotoURL, accessToChannel: [], isOnline: false, email: email });        
-      }else {
-        setDoc(doc(itemCollection), { uid: userId, fullName: fullName, photoURL: userPhotoURL, accessToChannel: [], isOnline: false, email: email }); 
+        setDoc(doc(itemCollection, userIsReg), { uid: userId, fullName: fullName, photoURL: userPhotoURL, accessToChannel: [], isOnline: false, email: email });
+      } else {
+        // setDoc(doc(itemCollection), { uid: userId, fullName: fullName, photoURL: userPhotoURL, accessToChannel: [], isOnline: false, email: email }) //// CHANGED BY TOM
+        this.crud.addItem(userData, environment.userDb)
+          .then((docRef) => {
+            this.upDateChannelUser(docRef)
+            // let updateItem = doc(collection(this.firestore, environment.channelDb), '8veqP2ohCvtLVgT46sP5');
+            // updateDoc(updateItem, {
+            //   ids: arrayUnion(docRef.id)
+            // });
+          });
       }
       this.setUserDataToLocalStorage(userId)
-      setTimeout(() => {this.route.navigateByUrl('');},200) 
+      setTimeout(() => { this.route.navigateByUrl(''); }, 200)
     });
-
   }
+
+
+  upDateChannelUser(docRef:any) {
+    let updateItem = doc(collection(this.firestore, environment.channelDb), '8veqP2ohCvtLVgT46sP5');
+    updateDoc(updateItem, {
+      ids: arrayUnion(docRef.id)
+    });
+  }
+
+
   signIn(email: string, password: string) {
-    
+
     signInWithEmailAndPassword(this.auth, email, password).then(resp => {
       this.setUserDataToLocalStorage(resp.user.uid)
       if (resp.user.emailVerified) {
-        setTimeout(() => {this.route.navigateByUrl('');},900)
+        setTimeout(() => { this.route.navigateByUrl(''); }, 900)
       } else {
         this.alert('Verifiziere Ihre E-Mail-Adresse!')
       }
@@ -67,23 +89,27 @@ export class AuthService {
       let code = err.code;
       code = code.slice(5);
       this.alert(code);
-      
+
     })
   }
+
+
   signInGuest() {
     signInAnonymously(this.auth).then(resp => {
       const userId: string = resp.user.uid
       const itemCollection = collection(this.firestore, environment.userDb);
       setDoc(doc(itemCollection), { uid: userId, fullName: 'Guest', photoURL: './assets/images/profile-icons/big/avatar-1.png', accessToChannel: [], isOnline: false });
       this.setUserDataToLocalStorage(userId)
-      setTimeout(() => {this.route.navigateByUrl('');},900)
+      setTimeout(() => { this.route.navigateByUrl(''); }, 900)
     });
   }
+
 
   logOut() {
     signOut(this.auth);
     this.route.navigateByUrl('auth/login');
   }
+
 
   setUserDataToLocalStorage(uid: string) {
     const itemCollection = collection(this.firestore, environment.userDb);
@@ -101,22 +127,25 @@ export class AuthService {
     })
   }
 
+
   getUserData(userDbId: string) {
     const itemDoc = doc(this.firestore, environment.userDb + '/' + userDbId);
     return docData(itemDoc)
   }
 
-  alert(text:string) {
+
+  alert(text: string) {
     this.alertService.setAlert(text, true);
   }
 
-  async isRegUser(userId:string):Promise<string> {
+
+  async isRegUser(userId: string): Promise<string> {
     const itemCollection = collection(this.firestore, environment.userDb);
     const q = query(itemCollection, where('uid', '==', userId));
     let isReg = '';
-    await getDocs(q).then((doc) => {      
+    await getDocs(q).then((doc) => {
       if (doc.docs.length == 1) {
-        isReg = doc.docs[0].id        
+        isReg = doc.docs[0].id
       }
     })
     return isReg
