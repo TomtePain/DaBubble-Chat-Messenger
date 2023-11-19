ec6aa9af2a347dbe77fd38f77f444744760f4e9c
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-11-19 12:58:08 +0100

    added storage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
     projectId: 'da-bubble-own',
     appId: '1:199785897732:web:45d55fca274013c6147a51',
     storageBucket: 'da-bubble-own.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyDmG43qF_eSj30iFKkpwel-lWiLGTUpcj8',
     authDomain: 'da-bubble-own.firebaseapp.com',
     messagingSenderId: '199785897732',
