896bf0651d4aabb6fb701226470d5b0cc021fc21
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-11-21 15:28:33 +0100

    chat responsive incl. workspace

--- src/app/main/components/workspace/workspace-main/workspace-main.component.ts
+++ src/app/main/components/workspace/workspace-main/workspace-main.component.ts
@@ -1,35 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { CrudService } from 'src/app/main/services/crud.service';
 import { TreeService } from 'src/app/main/services/tree.service';
 import { UserService } from 'src/app/main/services/user.service';
 
-
-
 @Component({
   selector: 'app-workspace-main',
   templateUrl: './workspace-main.component.html',
-  styleUrls: ['./workspace-main.component.scss']
+  styleUrls: ['./workspace-main.component.scss'],
 })
 export class WorkspaceMainComponent {
-  constructor(public firestore: Firestore, public crud: CrudService, public tree: TreeService, private userservice:UserService) {
+  workspace: boolean = true;
+  smallerDesktopWidth = 1570;
+  // channelPath = `MSuser/${this.userservice.userDBId}/channel`;
+  // channelItem = {
+  //   name: 'neuer Channel',
+  //   messages: [],
+  // };
 
-  }
-workspace:boolean=true;
-  channelPath = `MSuser/${this.userservice.userDBId}/channel`;
-  channelItem = {
-    name: 'neuer Channel',
-    messages: []
+  
+
+  constructor(
+    public firestore: Firestore,
+    public crud: CrudService,
+    public tree: TreeService,
+    private userservice: UserService
+  ) {}
+
+  ngOnInit() {
+    this.updateWorkspaceState(window.innerWidth);
   }
 
-  toggleWorkspace(){
-    this.workspace=!this.workspace;
+  //When the window is resized the innerWidth of the window is used to check if it is smaller than the smallerDesktopWidth. If it is smaller the workspace is set to false, so that the channel navigation gets hidden.
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.updateWorkspaceState(window.innerWidth);
   }
 
-  createNewMessage(){
+  toggleWorkspace() {
+    this.workspace = !this.workspace;
+  }
+
+  createNewMessage() {
     this.tree.newMessage = true;
   }
 
+  updateWorkspaceState(width: number) {
+    this.workspace = width > this.smallerDesktopWidth;
+  }
 }
 
 
