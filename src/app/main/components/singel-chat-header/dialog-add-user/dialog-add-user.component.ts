47effb2bc26bb431ffcbb85ea92c7a43bd2b85b2
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-12-05 11:08:30 +0100

    open Dialog for add user to exist channel

--- src/app/main/components/singel-chat-header/dialog-add-user/dialog-add-user.component.ts
+++ src/app/main/components/singel-chat-header/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { CrudService } from 'src/app/main/services/crud.service';
+import { AddedUserToChannel } from '../../workspace/add-people-to-channel/added-user-to-channel';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
-  styleUrls: ['./dialog-add-user.component.scss']
+  styleUrls: ['../../workspace/add-people-to-channel/add-people-to-channel.component.scss', './dialog-add-user.component.scss']
 })
-export class DialogAddUserComponent {
+export class DialogAddUserComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<DialogAddUserComponent>,
+    public dialog: MatDialog,
+    public firestore: Firestore,
+    public crud: CrudService) { }
+
+  channelName: string = '';
+  description: string = '';
+  channelPath: string = '';
+
+  addPeople: boolean = false;
+  searchTerm: string = '';
+  dmUsers: any = [];
+  addedToChannel: any = [];
+  addedToChannelIds: any = [];
+  searchUser: boolean = false;
+  uid: string = '';
+  placeholder: boolean = true;
+
+
+  ngOnInit(): void {
+
+  }
+
+
+  addUserToChannel(img: string, name: string, id: string) {
+    if (img && name && id) {
+      const addedUser = {
+        photoURL: img,
+        fullName: name,
+        uid: id,
+      };
+
+      const index = this.addedToChannel.findIndex((user: AddedUserToChannel) => (
+        user.photoURL === addedUser.photoURL &&
+        user.fullName === addedUser.fullName &&
+        user.uid === addedUser.uid
+      ));
+
+      if (index === -1) {
+        this.addedToChannel.push(addedUser);
+      }
+    }
+  }
+
+  removeFromAddList(i: number) {
+    this.addedToChannel.splice(i, 1);
+    console.log('test:', this.addedToChannel);
+
+  }
+
+  setPlaceholder() {
+    this.placeholder = false;
+  }
+
+
+  searchUserNew(event: any) {
+    const searchTerm = event.target.value;
+    this.dmUsers = [];
+
+    const userDocRef = this.crud.getItem(environment.userDb);
+    userDocRef.subscribe((response: any[]) => {
+      if (searchTerm !== '') {
+        this.searchUser = true;
+        this.dmUsers = response.filter((doc) =>
+          this.checkFieldsContainSearchTerm(doc, searchTerm) 
+        );    
+      } else {
+        this.searchUser = false;
+      }
+    });
+  }
+
+
+  private checkFieldsContainSearchTerm(doc: any, searchTerm: string): boolean {
+    // console.log('Search term is', searchTerm, 'Doc full name is', doc.fullName);
+    return doc.fullName.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+
 
 }
