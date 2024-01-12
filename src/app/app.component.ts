2ccd3d62240644d434c1efe65007d2cfc12f51cd
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2024-01-12 14:54:35 +0100

    search reset on click in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TreeService } from './main/services/tree.service';
+import { SearchService } from './main/services/search.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,11 @@ import { TreeService } from './main/services/tree.service';
 export class AppComponent {
   title = 'dabubble';
 
-  constructor(private tree: TreeService) {}
+  constructor(private tree: TreeService, private searchservice: SearchService) {}
 
   // Closes the navigation tree when clicking anywhere in the application. There is a hostlistener added in the workspace-main.component.ts that makes sure that the navigation tree can be opened by click and the navigation within can be clicked though the method below is included.
   onDocumentClick(): void {
     this.tree.closeWorkspace();
+    this.searchservice.clearSearch();
   }
 }
